ced38b0ec5c81dbaf7bcdb0d223883f21d1f6f67
gracesutton <gracesutton20@gmail.com>
2024-11-01 17:34:20 +1000

    Updated BubbleSort
    Read text file
    Created myNumbers[]

--- Week1/src/BubbleSort.java
+++ Week1/src/BubbleSort.java
@@ -4,10 +4,32 @@
 // Your program will read in a series of numbers from a text file (numbers.txt), sort them in ascending (descending) 
 // order and write them back to another file with the name numbers_sorted.txt.
 
+import java.io.*;
+import java.util.Scanner;
+
 public class BubbleSort {
     public static void main(String[] args) {
 
+        Integer[] myNumbers; 
 
-        
+        try {
+            // Reads the file using the Scanner class
+            File myFile = new File("Week1/src/numbers.txt");
+            Scanner input = new Scanner(myFile);
+
+            while (input.hasNextLine()) {
+                //  read expression from the keyboard as a string
+                Integer number = input.nextInt();
+
+                //???
+                
+            }
+            input.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+
     }
 }
