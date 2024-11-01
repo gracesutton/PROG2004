a4bd708da5dcb5a4596206b78e847926e30e807a
gracesutton <gracesutton20@gmail.com>
2024-11-01 14:40:55 +1000

    Created BubbleSort
    Created new BubbleSort file and initial class.

---
+++ Week1/src/BubbleSort.java
@@ -0,0 +1,13 @@
+// Activity 1.7 (Optional)
+
+// In this activity, you will develop a simple Java program to implement the bubble sort algorithm. 
+// Your program will read in a series of numbers from a text file (numbers.txt), sort them in ascending (descending) 
+// order and write them back to another file with the name numbers_sorted.txt.
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+
+        
+    }
+}
